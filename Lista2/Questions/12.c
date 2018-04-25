a96672fde2c7a28312d3f75c77687aa57d4b3d31
Lucas Agostinho Fernandes <36972783+LucasAgosti@users.noreply.github.com>
2018-04-25 17:43:45 -0300

    Lista 2

---
+++ Lista2/Questions/12.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+
+    int i, N;
+
+    printf("Digite um número N\n");
+    scanf("%d", &N);
+
+    for(i = 0; i <= N; i++){
+      printf("%d\n", i);
+    }
+
+    return 0;
+}
