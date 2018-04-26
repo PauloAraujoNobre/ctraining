36d83c1ea2f48cbeb84d8b24972090132aea7bdb
Lucas Agostinho Fernandes <36972783+LucasAgosti@users.noreply.github.com>
2018-04-26 19:47:38 -0300

    URI

---
+++ URI- online judge/1011/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define PI 3.14159
+int main() {
+ 
+    double raio;
+    
+    scanf("%lf", &raio);
+    printf("VOLUME = %.3lf\n", 4.0/3.0 * PI * raio * raio * raio);
+ 
+    return 0;
+}
