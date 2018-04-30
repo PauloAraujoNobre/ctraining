f0eaaefc47ec97d9422b58863aea604376e53ec3
Lucas Agostinho Fernandes <36972783+LucasAgosti@users.noreply.github.com>
2018-04-30 14:49:36 -0300

    URI

---
+++ URI- online judge/1037/main.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    double intervalo;
+
+    scanf("%lf", &intervalo);
+
+    if(intervalo < 0){
+       printf("Fora de intervalo\n");
+       
+    }else if(intervalo <= 25){
+       printf("Intervalo [0,25]\n");
+       
+    }else if(intervalo <= 50){
+       printf("Intervalo (25,50]\n");	
+       
+    }else if(intervalo <= 75){
+       printf("Intervalo (50,75]\n");
+       
+    }else if(intervalo <= 100){
+       printf("Intervalo (75,100]\n");
+       
+    }else{
+       printf("Fora de intervalo\n");
+    }
+
+	  return 0;
+
+}
