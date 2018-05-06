07537967d42b0ef06c0ec35815e62a008fa204e6
Lucas Agostinho Fernandes <36972783+LucasAgosti@users.noreply.github.com>
2018-05-06 16:15:01 -0300

    URI

---
+++ URI- online judge/1043/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    float A, B, C, area;
+
+    scanf("%f %f %f", &A, &B, &C);
+
+    if(((A + B) > C) && ((A + C) > B) && ((B + C) > A)){
+        printf("Perimetro = %.1f\n", A + B + C);
+
+    }else{
+        printf("Area = %.1f\n", ((A + B) * C )/ 2);
+
+    }
+
+    return 0;
+}
